7f18b456b21694c0c3356377b73882194f31a653
phihungvo <vophihung987@gmail.com>
2024-10-06 00:05:27 +0700

    second commit

---
+++ src/test/java/com/luv2code/demo1/Demo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.demo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
